a10ef1668da3c116e5c68049092c78b0c3068dce
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-04-23 19:13:17 +0300

    висвітення всії типів в оду строку

---
+++ less10.java
@@ -0,0 +1,13 @@
+public class less10 {
+
+    public static void main(String args[]){
+
+        String name = "Amigo";
+        int age = 12;
+        String friend = "Diego";
+        int weight = 200;
+
+        System.out.println("User = {name: " + name + ", age:" + age + " years, friend: " + friend+", weight: " + weight + " kg.}");
+
+    }
+}
