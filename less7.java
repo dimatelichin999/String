83dc831789b545e43d41060a612d4f778137f3fc
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-04-23 18:56:07 +0300

    зміна букв , показання не всіх елемнтів

--- less7.java
+++ less7.java
@@ -1,4 +1,15 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class less7 {
 
-    
+    public static void main(String args[]) {
+
+        System.out.println("привіт".substring(0, 3));
+
+        System.out.println("привіт".replace("п", "П"));
+
+        System.out.println("привіт".trim());
+
+    }
 }
