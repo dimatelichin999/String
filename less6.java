a48dad8383b69f8b72e7577cdd3894f0ac559f3c
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-04-23 18:45:21 +0300

    показання строк

---
+++ less6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class less6 {
+
+    public static void main (String args[]){}
+
+
+    String str = "Дослідником";
+
+    String x = str.substring(0,10);
+
+
+
+
+}
+
