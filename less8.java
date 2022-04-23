9eb28dd8fe719ae4320233157efe34b4a1ab7fe2
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-04-23 19:08:00 +0300

    видаленя частини тексту

---
+++ less8.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class less8 {
+
+    public static void main(String args[]){
+
+        String str = "Good morning, everyone";
+        String [] strings = str.split("ne");
+        System.out.println(Arrays.toString(strings));
+
+        
+    }
+}
